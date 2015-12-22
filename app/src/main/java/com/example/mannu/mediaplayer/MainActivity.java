34344270be0477feb16ef40e891b1d8923a12713
chetancodev95 <chetanmann95@gmail.com>
2015-12-22 16:02:21 +0530

    code changed

--- app/src/main/java/com/example/mannu/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mannu/mediaplayer/MainActivity.java
@@ -1,108 +1,65 @@
 package com.example.mannu.mediaplayer;
 
-import android.content.res.AssetFileDescriptor;
+import android.app.ListActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
+import java.util.List;
+
+class Mp3Filter implements FilenameFilter{
+    public boolean accept(File dir, String name){
+        return(name.endsWith(".mp3") || name.endsWith(".MP3"));
+    }
+}
 
 
-public class MainActivity extends ActionBarActivity {
-ListView lv;
-    String[] items;
-    static MediaPlayer mp;
+public class MainActivity extends ListActivity {
+    private ListView lv;
+
+    private static final String SD_PATH = new String("/sdcard/");
+
+    Button stop;
+
+    private List<String> songs = new ArrayList<>();
+    private MediaPlayer mp = new MediaPlayer();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        updatePlaylist();
+
         lv=(ListView)findViewById(R.id.lvPlayList);
+        stop = (Button)findViewById(R.id.buttonStop);
 
-
-        final ArrayList<File> mySongs = findSongs(Environment.getExternalStorageDirectory());
-        items = new String[mySongs.size()];
-        for(int i =0;i<mySongs.size();i++){
-            //toast(mySongs.get(i).getName().toString());
-            items[i] = mySongs.get(i).getName().toString().replace(".mp3","").replace(".wav","");
-        }
-        ArrayAdapter<String> adp = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,items);
-        lv.setAdapter(adp);
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        stop.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //startActivity(new Intent(getApplicationContext(),PlayerActivity.class).putExtra("pos",position).putExtra("songlist",mySongs));
-                if(mp.isPlaying())
-                {
-                    mp.stop();
-                }
-
-                try {
-                    mp.reset();
-                    AssetFileDescriptor afd;
-                    afd = getAssets().openFd("Audio.mp3");  //Note that AudioFile.mp3 is the name of the mp3 file in /assets folder
-                   // afd = getAssets().openFd(mySongs.get(position).getName());
-                    mp.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
-                    mp.prepare();
-                    mp.start();
-                } catch (IllegalStateException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            public void onClick(View v) {
+                mp.stop();
             }
         });
+
     }
 
-    public ArrayList<File> findSongs(File root){
-        ArrayList<File> a1 = new ArrayList<File>();
-        File[] files = root.listFiles();
-        for(File singleFile : files){
-            if(singleFile.isDirectory() && !singleFile.isHidden()){
-                a1.addAll(findSongs(singleFile));
-            }
-            else{
-                if(singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")){
-                   a1.add(singleFile);
-                }
-            }
+    public void updatePlaylist(){
+
+       // File home = new File(String.valueOf(Environment.getExternalStorageDirectory()));      //This can b used for all files not only sd_card
+
+        File home = new File(SD_PATH);
+        if(home.listFiles(new Mp3Filter()).length>0){
+            for (File file : home.listFiles(new Mp3Filter()))
+                songs.add(file.getName());
+            ArrayAdapter<String> songList = new ArrayAdapter<String>(this,R.layout.activity_player,songs);
+            setListAdapter(songList);
         }
-        return a1;
     }
 
-//    public void toast(String text){
-//        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
-//    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
